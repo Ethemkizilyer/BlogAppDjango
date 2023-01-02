78c8d53cad01609482eeb2ce56df01e166e61a6b
Ethm KZLYR <106928233+Ethemkizilyer@users.noreply.github.com>
2023-01-02 20:52:27 +0300

    blog3

--- blog/models.py
+++ blog/models.py
@@ -8,3 +8,12 @@ class Category(models.Model):
         return self.name
     
 class Blog(models.Model):
+    title= models.CharField(max_length=100)
+    content= models.Textfield(blank=True)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    is_published=models.BooleanField(default=False)
+    created_date= models.DateTimeField(auto_now_add=True)
+    updated_date=models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
