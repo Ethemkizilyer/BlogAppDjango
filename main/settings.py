57ab454bc37cfea0e52fdf3dcbc5842cc355f3da
Ethm KZLYR <106928233+Ethemkizilyer@users.noreply.github.com>
2023-01-04 02:50:21 +0300

    permission and user add

--- main/settings.py
+++ main/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     # third party
     'rest_framework',
     "django_filters",
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -135,6 +136,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend','rest_framework.filters.SearchFilter',],
     'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework.authentication.TokenAuthentication']
 }
